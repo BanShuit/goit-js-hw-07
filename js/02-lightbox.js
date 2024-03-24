e6f0ae0d80e2de23afcda52453e0961e328539bf
BanShuit <tuna125@o2.pl>
2024-03-24 16:56:32 +0100

    update

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+document.addEventListener("DOMContentLoaded", function () {
+    const galleryContainer = document.querySelector('.gallery');
+
+    galleryItems.forEach(item => {
+        const galleryItem = document.createElement('li');
+        galleryItem.classList.add('gallery__item');
+
+        const link = document.createElement('a');
+        link.classList.add('gallery__link');
+        link.href = item.original;
+
+        const image = document.createElement('img');
+        image.classList.add('gallery__image');
+        image.src = item.preview;
+        image.alt = item.description;
+
+        link.appendChild(image);
+        galleryItem.appendChild(link);
+        galleryContainer.appendChild(galleryItem);
+    });
+
+    // Inicjalizacja biblioteki SimpleLightbox
+    new SimpleLightbox('.gallery__item a', {
+        captionsData: 'alt',
+        captionDelay: 250
+    });
+});
